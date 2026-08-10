2e7d1add30865ec49e4de554526c70d37dcce919
shumbabumba <195567606+shumbabumba@users.noreply.github.com>
2026-08-10 18:35:07 +0400

    map displays location ids

--- ghostbook.js
+++ ghostbook.js
@@ -9,18 +9,16 @@ fetch("files/locations.json")
     .then(response => response.json())
     .then(locations => {
         for (const location of locations) {
-            const marker = L.marker([location.latitude, location.longitude]).addTo(map);
-            let popup = `
-                <b>${location.title}</b><br>
-                Date: ${location.date}<br>
-                `;
-
-            for(let i = 0; i < location.imageCount; i++)
-            {
-                popup += `
-                <img src="images/${location.id}_${i}.jpg">`;
-            }
-
-            marker.bindPopup(popup);
+            const textIcon = L.divIcon({
+            className: 'custom-text-marker',
+            html: `<div style="color: white; font-weight: bold;">${location.id}</div>`,
+            iconSize: [100, 40],
+            iconAnchor: [50, 20]
+            });
+        L.marker([location.latitude, location.longitude], {
+                icon: textIcon 
+            }).addTo(map);
         }
     });
+
+    
