ba2dd95e9ce7c4269074f09576599f12eec7cbcc
shumbabumba <195567606+shumbabumba@users.noreply.github.com>
2026-08-15 17:10:30 +0400

    new design

--- ghostbook.js
+++ ghostbook.js
@@ -1,24 +1,10 @@
-var map = L.map('map').setView([40.394622345395554, 49.84926027776045], 12);
+var map = L.map('map', { zoomControl: false,     
+    attributionControl: false
+}).setView([40.394622345395554, 49.84926027776045], 12);
 
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
+    maxZoom: 20
 }).addTo(map);
 
-fetch("files/locations.json")
-    .then(response => response.json())
-    .then(locations => {
-        for (const location of locations) {
-            const textIcon = L.divIcon({
-            className: 'custom-text-marker',
-            html: `<div style="color: white; font-weight: bold;">${location.id}</div>`,
-            iconSize: [100, 40],
-            iconAnchor: [50, 20]
-            });
-        L.marker([location.latitude, location.longitude], {
-                icon: textIcon 
-            }).addTo(map);
-        }
-    });
 
     
